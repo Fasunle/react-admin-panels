bb8fe2af104a7aabd2f6a5468b57f6dfe0e5a74d
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-11 09:02:46 +0100

    [Refactoring] AdminProfile -> Admin

---
+++ src/components/Header/Admin.jsx
@@ -0,0 +1,14 @@
+import { Menu } from '@mui/material';
+
+export const Admin = (props) => {
+    return (
+        <Menu
+        {...props}
+      >
+        Hello from Menu Layer
+
+      </Menu>
+    )
+}
+
+export default Admin;
