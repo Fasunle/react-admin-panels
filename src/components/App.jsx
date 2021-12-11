7e28e445b1af7e9d96852cdbf4a99d443882bf6d
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2021-12-11 08:45:32 +0100

    [Router] wrapped all components with Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Container, CssBaseline} from '@mui/material';
+import { Stack , CssBaseline} from '@mui/material';
 import {ThemeProvider} from '@mui/material/styles';
-
+import { BrowserRouter as Router } from "react-router-dom";
 // local imports
-import NavBar from './NavBar';
+import Header from './Header';
 import theme from '../theme';
+import SidebarNavigation from "./Sidebar";
+import Content from "./Contents";
 
 const App = () => {
     return (
         <ThemeProvider theme={theme} >
-            <CssBaseline/>
-            <NavBar />
+            <CssBaseline/>      {/**override any browser default */}
+            <Router>
+                <Header />
+                <Stack direction="row" sx={{marginTop: '5rem'}}>
+                    <SidebarNavigation />
+                    <Content/>
+                </Stack>
+            </Router>
         </ThemeProvider>
     );
 }
